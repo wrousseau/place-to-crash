954dfdb7fa8855f4820b836b2c5d7709be9066d5
Woody Rousseau <woody.rousseau@ensta-paristech.fr>
2014-05-13 00:49:02 +0200

    Finished client side validation with email and username uniqueness checks

--- assets/public/user/signup.js
+++ assets/public/user/signup.js
@@ -2,50 +2,54 @@ $(function () {
     $('.button-checkbox').each(function () {
         // Settings
         var $widget = $(this),
-            $button = $widget.find('button'),
-            $checkbox = $widget.find('input:checkbox'),
-            color = $button.data('color'),
-            settings = {
-                on: {
-                    icon: 'glyphicon glyphicon-check'
-                },
-                off: {
-                    icon: 'glyphicon glyphicon-unchecked'
-                }
-            };
+        $button = $widget.find('button'),
+        $checkbox = $widget.find('input:checkbox'),
+        color = $button.data('color'),
+        settings = {
+            on: {
+                icon: 'glyphicon glyphicon-check'
+            },
+            off: {
+                icon: 'glyphicon glyphicon-unchecked'
+            }
+        };
 
         // Event Handlers
         $button.on('click', function () {
-            $checkbox.prop('checked', !$checkbox.is(':checked'));
+            $checkbox.prop('checked', !$checkbox.prop('checked'));
             $checkbox.triggerHandler('change');
             updateDisplay();
         });
         $checkbox.on('change', function () {
             updateDisplay();
         });
-
+        $(".modal-footer button").on('click', function() {
+            $checkbox.prop('checked', true);
+            $("button:submit").prop("disabled", false);
+            updateDisplay();
+        })
         // Actions
         function updateDisplay() {
-            var isChecked = $checkbox.is(':checked');
+            var isChecked = $checkbox.prop('checked');
 
             // Set the button's state
             $button.data('state', (isChecked) ? "on" : "off");
 
             // Set the button's icon
             $button.find('.state-icon')
-                .removeClass()
-                .addClass('state-icon ' + settings[$button.data('state')].icon);
+            .removeClass()
+            .addClass('state-icon ' + settings[$button.data('state')].icon);
 
             // Update the button's color
             if (isChecked) {
                 $button
-                    .removeClass('btn-default')
-                    .addClass('btn-' + color + ' active');
+                .removeClass('btn-default')
+                .addClass('btn-' + color + ' active');
             }
             else {
                 $button
-                    .removeClass('btn-' + color + ' active')
-                    .addClass('btn-default');
+                .removeClass('btn-' + color + ' active')
+                .addClass('btn-default');
             }
         }
 
@@ -62,77 +66,123 @@ $(function () {
         init();
     });
 
-    
-    $('#signupForm')
-        .bootstrapValidator({
-            message: 'Ce champs est invalide',
-            feedbackIcons: {
-                valid: 'glyphicon glyphicon-ok',
-                invalid: 'glyphicon glyphicon-remove',
-                validating: 'glyphicon glyphicon-refresh'
+    $('#signupForm').bootstrapValidator({
+        message: 'Ce champs est invalide',
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        live: 'enabled',
+        submitButtons: 'button[type="submit"]',
+        submitHandler: function(validator, form, submitButton) {
+            if (!$("input:checkbox").prop("checked"))
+            {
+                $('#t_and_c_m').modal();
+                $
+                return;
+            }
+            if ($(form).isValid())
+                alert("ok");
+            validator.defaultSubmit();
+        },
+        fields: {
+            firstName: {
+                validators: {
+                    notEmpty: {
+                        message: 'Le prénom est requis'
+                    }
+                }
             },
-            submitHandler: function(validator, form, submitButton) {
-                alert("test");
+            lastName: {
+                validators: {
+                    notEmpty: {
+                        message: 'Le nom de famille est requis'
+                    }
+                }
             },
-            fields: {
-                firstName: {
-                    validators: {
-                        notEmpty: {
-                            message: 'Le prénom est requis'
+            username: {
+                validators: {
+                    notEmpty: {
+                        message: "Le nom d'utilisateur est requis"
+                    },
+                    callback: {
+                        message: "Le nom d'utilisateur est déjà utilisé",
+                        callback: function(input, validator) {
+                            $.ajaxSetup({
+                                async: false
+                            });
+                            var found = false;
+                            $.get("user/find", function(data) {
+                                $.each(data, function (index, value) {
+                                    if (value.n.data.username === input)
+                                        found = true;
+                                });
+                            });
+                            $.ajaxSetup({
+                                async: true
+                            });
+                            return !found;
                         }
+                    },
+                    stringLength: {
+                        min: 5,
+                        max: 30,
+                        message: "Le nom d'utilisateur doit comprendre entre 5 et 30 caractères"
                     }
-                },
-                lastName: {
-                    validators: {
-                        notEmpty: {
-                            message: 'Le nom de famille est requis'
-                        }
-                    }
-                },
-                username: {
-                    validators: {
-                        notEmpty: {
-                            message: "Le nom d'utilisateur est requis"
-                        }
-                    }
-                },
-                email: {
-                    validators: {
-                        notEmpty: {
-                            message: "L'email est requis"
-                        },
-                        emailAddress: {
-                            message: "L'email entré n'est pas un email valide"
-                        }
-                    }
-                },
-                password: {
-                    validators: {
-                        notEmpty: {
-                            message: "Le mot de passe est requis"
-                        },
-                        different: {
-                            field: 'username',
-                            message: "Le mot de passe ne peut être identique au nom d'utilisateur"
-                        }
-                    }
-                },
-                passwordConfirmation: {
-                    validators: {
-                        notEmpty: {
-                            message: "Le mot de passe est requis"
-                        },
-                        identical: {
-                            field: 'password',
-                            message: "Les deux mots de passe ne correspondent pas"
+                }
+            },
+            email: {
+                validators: {
+                    notEmpty: {
+                        message: "L'email est requis"
+                    },
+                    emailAddress: {
+                        message: "L'email entré n'est pas un email valide"
+                    },
+                    callback: {
+                        message: "L'email entré est déjà utilisé",
+                        callback: function(input, validator) {
+                            $.ajaxSetup({
+                                async: false
+                            });
+                            var found = false;
+                            $.get("user/find", function(data) {
+                                $.each(data, function (index, value) {
+                                    if (value.n.data.email === input)
+                                        found = true;
+                                });
+                            });
+                            $.ajaxSetup({
+                                async: true
+                            });
+                            return !found;
                         }
                     }
                 }
-            }
-        })
-        .find('button[data-toggle]')
-            .on('click', function() {
-                var $target = $($(this).attr('data-toggle'));
-                alert("test");
-        });
+            },
+            password: {
+                validators: {
+                    notEmpty: {
+                        message: "Le mot de passe est requis"
+                    },
+                    different: {
+                        field: 'username',
+                        message: "Le mot de passe ne peut être identique au nom d'utilisateur"
+                    }
+                }
+            },
+            passwordConfirmation: {
+                validators: {
+                    notEmpty: {
+                        message: "Le mot de passe est requis"
+                    },
+                    identical: {
+                        field: 'password',
+                        message: "Les deux mots de passe ne correspondent pas"
+                    }
+                }
+            },
+        }
+    });
 });
