9b754d4d1df8a07a9af90c594054a6158f9d5a88
Sammy Teillet <sammy.teillet@gmail.com>
2014-05-16 12:16:07 +0200

    with socket lol

--- assets/public/user/index.js
+++ assets/public/user/index.js
@@ -1,5 +1,5 @@
-$(function() {
-	$.get('user/find', function(data){
+$(function(socket) {
+	socket.get('user/find', function(data){
 		$.each(data, function (index, value) {
 			$( ".tbody" ).append(
 				"<tr>" +
