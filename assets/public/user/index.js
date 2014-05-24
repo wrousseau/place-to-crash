2a0078898138ce986d53437368cfd17cbc37e295
Woody Rousseau <woody.rousseau@ensta-paristech.fr>
2014-05-24 16:53:55 +0200

    Added icon for online/offline status in the user list

--- assets/public/user/index.js
+++ assets/public/user/index.js
@@ -1,8 +1,12 @@
 $(function(socket) {
 	socket.get('/user/find', function(data){
 		$.each(data, function (index, value) {
+			var userStatusCell = '<td class="user-status"><span class="glyphicon ';
+			userStatusCell += (value.online) ? 'glyphicon-ok' : 'glyphicon-remove';
+			userStatusCell += '"></span>';
 			$( ".tbody" ).append(
 				"<tr>" +
+				userStatusCell + 
 	            "<td>" + value.firstName + "</td>" +
 	            "<td>" + value.lastName + "</td>" +
 	            "<td>" + value.username + "</td>" +
